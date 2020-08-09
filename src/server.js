5aedc0beb7ad71756d2d12f55872aef01663d21e
barbara <6abi.cardoso@gmail.com>
2020-08-09 14:51:49 -0300

    uso do template end para atualizar o front de forma dinamica pegando objetos do backend

--- src/server.js
+++ src/server.js
@@ -1,50 +1,61 @@
 
 // //alimentando com dados direto no back
-// const proffys = [
-//     {
-//         name: "Diego Fernandes",
-//         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-//         whatsapp:90888-8888,
-//         bio: "Entusiasta das melhores tecnologias de química avançada."
-//             "Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências."
-//             "Mais de 200.000 pessoas já passaram por uma das minhas explosões",
-//         subject: "Química.",
-//         cost:"20",
-//         weekday:[0],
-//         time_from:[720],
-//         time_to:[1220]
-//     },
-//     {
-//         name: "Daniele Silvas",
-//         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-//         whatsapp:90888-8888,
-//         bio: "Entusiasta das melhores tecnologias de química avançada."
-//             "Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências."
-//             "Mais de 200.000 pessoas já passaram por uma das minhas explosões",
-//         subject: "Química.",
-//         cost:"20",
-//         weekday:[1],
-//         time_from:[720],
-//         time_to:[1220]
-//     }
-// ]
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp:90888-8888,
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química.",
+        cost:"20",
+        weekday:[0],
+        time_from:[720],
+        time_to:[1220]
+    },
+    {
+        name: "Daniele Silvas",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp:90888-8888,
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+        subject: "Química.",
+        cost:"20",
+        weekday:[1],
+        time_from:[720],
+        time_to:[1220]
+    }
+]
 
-const express = require('express')     //chamada função express que retorna um objeto
+//funções das pages - renderizar
+function pageLanding(req, res){
+    return res.render("index.html")
+}
+
+function pageStudy(req, res){
+    return res.render("study.html", {proffys})
+
+}
+
+function pageGiveClasses(req, res){
+    return res.render("give-classes.html")
+
+}
+
+const express = require('express')     
 const server = express()
+const nunjucks = require('nunjucks')
 
-server.use(express.static("public"))
 
-.get("/", (req, res) => {                      //arrow function, para direcionar para a pagina setada
-    return res.sendFile(__dirname + "/views/index.html")
-
-})
-.get("/study", (req, res) => {                      //arrow function, para rotear para a pagina setada
-    return res.sendFile(__dirname + "/views/study.html")
-
-})
-.get("/give-classes", (req, res) => {                      //arrow function, para direcionar para a pagina setada
-    return res.sendFile(__dirname + "/views/give-classes.html")
-
+//configurar nunjucks
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
+server
+//configuração dos arquivos estaticos
+.use(express.static("public"))
+//passagem das funções para o get, roteamento da aplicação
+.get("/", pageLanding)                    
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
