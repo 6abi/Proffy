17f4a0a1bd2889dd3c0a81d2a336c6237f1a265d
barbara <6abi.cardoso@gmail.com>
2020-08-09 01:42:55 -0300

    Alimentando o backend com dados para mostrar no front

--- src/server.js
+++ src/server.js
@@ -1,3 +1,34 @@
+
+// //alimentando com dados direto no back
+// const proffys = [
+//     {
+//         name: "Diego Fernandes",
+//         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+//         whatsapp:90888-8888,
+//         bio: "Entusiasta das melhores tecnologias de química avançada."
+//             "Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências."
+//             "Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+//         subject: "Química.",
+//         cost:"20",
+//         weekday:[0],
+//         time_from:[720],
+//         time_to:[1220]
+//     },
+//     {
+//         name: "Daniele Silvas",
+//         avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+//         whatsapp:90888-8888,
+//         bio: "Entusiasta das melhores tecnologias de química avançada."
+//             "Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências."
+//             "Mais de 200.000 pessoas já passaram por uma das minhas explosões",
+//         subject: "Química.",
+//         cost:"20",
+//         weekday:[1],
+//         time_from:[720],
+//         time_to:[1220]
+//     }
+// ]
+
 const express = require('express')     //chamada função express que retorna um objeto
 const server = express()
 
@@ -15,4 +46,5 @@ server.use(express.static("public"))
     return res.sendFile(__dirname + "/views/give-classes.html")
 
 })
+
 .listen(5500)
